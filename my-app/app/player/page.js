9726012ed814fb57e69e02570be55ea61bc887ed
tkhs-0114 <135799380+tkhs-0114@users.noreply.github.com>
2025-02-17 17:37:36 +0900

    devブランチとマージ

--- my-app/app/player/page.js
+++ my-app/app/player/page.js
@@ -1,15 +1,25 @@
 "use client"
 import { useSession } from "next-auth/react"
-import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
 
 export default function Home() {
     const { data: session } = useSession();
     console.log(session);
     const token = session?.token?.access_token;
 
+    const SimpleMap = useMemo(
+        () =>
+            dynamic(() => import('./simpleMap'), {
+                loading: () => <p>map loading...</p>,
+                ssr: false,
+            }),
+        []
+    );
+
     return (
         <>
-            <p>再生するページ</p>
+            <SimpleMap />
         </>
     )
 }
