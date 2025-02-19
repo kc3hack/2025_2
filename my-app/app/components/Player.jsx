bf6fe90da58d6463804b396411b27dc07cc66b9d
tkhs-0114 <135799380+tkhs-0114@users.noreply.github.com>
2025-02-19 11:21:04 +0900

    再生画面のUIを追加

--- my-app/app/components/Player.jsx
+++ my-app/app/components/Player.jsx
@@ -1,4 +1,8 @@
+"use client"
+import { useEffect } from "react";
+
 export default function Player() {
+    // ここで現在、再生中
     return (
         <div className="fixed bottom-0 left-0 w-full h-32 bg-blue-900 text-white flex items-center px-6">
             {/* 曲のアイコン */}
@@ -7,6 +11,20 @@ export default function Player() {
                 alt="Song Thumbnail"
                 className="w-16 h-16 rounded-md object-cover bg-white"
             />
+            <div className="flex-1 ml-4">
+                {/* 曲名 */}
+                <div className="text-lg font-bold">Song Title</div>
+                {/* アーティスト名 */}
+                <div className="text-sm">Artist Name</div>
+            </div>
+            {/* 再生ボタン */}
+            <button className="w-16 h-16 bg-white rounded-full flex items-center justify-center">
+                <img
+                    src="play-solid.svg"
+                    alt="Play Button"
+                    className="w-8 h-8"
+                />
+            </button>
         </div>
     );
 }
