362f129ef1f1d934db4c35f956c830a8e653ceab
uciyama886 <xneishan51@gmail.com>
2025-02-20 11:09:33 +0900

    [Add]:ログイン時にUserTable登録機能の追加

--- my-app/app/login/page.js
+++ my-app/app/login/page.js
@@ -1,21 +1,37 @@
 "use client"
-import { useSession } from "next-auth/react"
-import { signIn, signOut } from "next-auth/react"
+
+import { useSession } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function Home() {
     const { data: session } = useSession();
-    console.log(session);
     const token = session?.token?.access_token;
+
+    useEffect(() => {
+        if (session) {
+            console.log("Session data:", session);
+        }
+    },[session]);
+
     if (session) {
         return (
-            <div className='p-6'>
-                <span className='bold-txt'>{token}</span>
-                <p className='opacity-70 mt-8 mb-5 underline cursor-pointer' onClick={() => signOut()}>Sign Out</p>
+            <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+                <div className="p-6 rounded-lg shadow-lg bg-white max-w-xs w-full text-center">
+                    <span className="block text-black font-bold mb-4">{token}</span>
+                    <p className="opacity-70 underline cursor-pointer text-blue-500" onClick={() => signOut()}>Sign Out</p>
+                </div>
             </div>
-        )
+        );
     } else {
         return (
-            <button onClick={() => signIn()} className='shadow-primary w-56 h-16 rounded-xl bg-white border-0 text-black text-3xl active:scale-[0.99] m-6'>Sign In</button>
-        )
+            <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+                <button 
+                    onClick={() => signIn()} 
+                    className="w-64 h-16 rounded-full bg-white border-2 border-gray-300 text-black text-3xl shadow-lg active:scale-[0.99] transform transition-transform">
+                    Sign In
+                </button>
+            </div>
+        );
     }
 }
