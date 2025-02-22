6aefc49237bd2ae682499399e49bb08c97278aaf
uciyama886 <xneishan51@gmail.com>
2025-02-22 10:44:48 +0900

    [add]:playerページで最も近い曲をCollectionTableに追加する機能を追加

--- my-app/app/player/page.js
+++ my-app/app/player/page.js
@@ -1,15 +1,164 @@
 "use client"
 import { useSession } from "next-auth/react"
-import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic';
+import { useMemo, useState, useEffect } from 'react';
+import Player from "../components/player";
+
+/* blockNoにある音楽をGETする */
+async function getMusic(blockNo) {
+    const res = await fetch(`/api/player?blockNo=${blockNo}`);
+    const musics = await res.json();
+    console.log(blockNo);
+    return musics;
+}
 
 export default function Home() {
     const { data: session } = useSession();
-    console.log(session);
     const token = session?.token?.access_token;
+
+    const [position, setPosition] = useState([35.0127, 135.7094305]);
+    const [musics, setMusics] = useState([]);
+    const [blockNo, setBlockno] = useState(calcBlockNo(position[0], position[1]));
+    const [closestSpot, setClosestSpot] = useState(null);
+
+    const SimpleMap = useMemo(
+        () =>
+            dynamic(() => import('./simpleMap'), {
+                loading: () => <p>map loading...</p>,
+                ssr: false,
+            }),
+        []
+    );
+
+    /* BlockNoを計算する */
+    function calcBlockNo(Latitude, Longitude) {
+        return (Math.floor((Latitude + 90) / 0.01) * 2700 + Math.floor((Longitude + 180) / 0.01));
+    }
+
+    /* コンポーネントがレンダリングされたときに実行される */
+    useEffect(() => {
+        console.log("Start");
+        /* 繰り返されて実行される関数 */
+        const loop = setInterval(() => {
+
+            /* 現在地の緯度経度からBlockNumや最も近いスポットの更新、CollectionTableへの登録をする */
+            navigator.geolocation.getCurrentPosition(async (posi) => {
+
+                /* 現在地の緯度経度を取得する */
+                const newPosi = [posi.coords.latitude, posi.coords.longitude]; //新たな現在地情報の緯度と経度を取得
+                console.log(newPosi); //新たな現在地を表示
+                setPosition(newPosi); //現在地を更新
+
+                /* 現在地からBlockNoを計算、更新する */
+                const newBlockNo = calcBlockNo(newPosi[0], newPosi[1]); //現在地のBlockNoを計算
+                if (newBlockNo !== blockNo) { //BlockNoが変化した場合
+                    setBlockno(newBlockNo); //BlockNoを更新
+                    const musics = await getMusic(newBlockNo); //BlockNoにある音楽を新たに取得
+                    setMusics(musics); //BlockNoにある音楽を更新
+                }
+
+                /* 現在地と最も近いスポットの曲を導く */
+                let minDistance = Infinity; //最短距離
+                let newClosestSpot = null; //新しい最短スポット
+                for (const music of musics) {
+                    const newDistance = calcVincentyDistance(newPosi[0], newPosi[1], music.Latitude, music.Longitude);
+                    if (minDistance > newDistance) { //より近いスポットの場合
+                        minDistance = newDistance; //最短スポット更新
+                        newClosestSpot = music; //最短スポットの音楽更新
+                    }
+                }
+
+                /* 最も近いスポットの曲をCollectionTableに保存 */
+                if (closestSpot !== newClosestSpot) { //最も近いスポットが変更された場合
+                    setClosestSpot(newClosestSpot); //最も近いスポットを更新
+
+                    const saveMusic = async () => {
+                        const response = await fetch('/api/collectiondb/route', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                EmailAdd: session.user.email,
+                                MusicID: newClosestSpot.music.MusicID,
+                            }),
+                        });
+                        if (!response.ok) {
+                            console.log('Failed to save music');
+                        }
+                    };
+                    saveMusic();
+                }
+
+            }, 1000); //1000ms(1s)毎に繰り返す
+
+            return () => clearInterval(loop); //コンポーネントがアンマウントされたときにsetIntervalの中身を消去
+        }, [blockNo]); //blockNoに依存して実行される
+    }, [blockNo]);
+
+    /* 度からラジアンに */
+    function toRadians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+    /* Vincenty法 */
+    function calcVincentyDistance(lat1, lon1, lat2, lon2) { //始点の緯度・経度、終点の緯度・経度から計算
+        const a = 6378137.0; // WGS-84 長半径（赤道半径）（メートル）
+        const f = 1 / 298.257223563; // 扁平率
+        const b = (1 - f) * a; //短半径を計算
+
+        const φ1 = toRadians(lat1); //始点の緯度をラジアンに
+        const φ2 = toRadians(lat2); //終点の緯度をラジアンに
+        const L = toRadians(lon2 - lon1); //経度差をラジアンに
+
+        const U1 = Math.atan((1 - f) * Math.tan(φ1)); //修正された始点の緯度を計算
+        const U2 = Math.atan((1 - f) * Math.tan(φ2)); //修正された終点の緯度を計算
+        const sinU1 = Math.sin(U1); //修正された始点の緯度の正弦と
+        const cosU1 = Math.cos(U1); //余弦を計算
+        const sinU2 = Math.sin(U2); //修正された終点の緯度の正弦と
+        const cosU2 = Math.cos(U2); //余弦を計算
+
+        let λ = L; //初期方位格差を初期化
+        let λP; //前の方位角差の変数を初期化
+        let iterLimit = 100; //反復計算の上限を設定
+        let cosSqα, sinσ, cos2σM, cosσ, σ; //中間計算のための変数を初期化
+
+        /* 反復計算 */
+        do {
+            const sinλ = Math.sin(λ); //方位角の正弦と
+            const cosλ = Math.cos(λ); //余弦を計算
+            // sinσ = Math.sqrt((cosU2 * sinλ) * (cosU2 * sinλ) + (cosU1 * sinU2 - sinU1 * cosU2 * cosλ) * (cosU1 * sinU2 - sinU1 * cosU2 * cosλ)); //修正された距離の正弦を計算
+            if (sinσ === 0) return 0; //共通点の場合の処理
+            cosσ = sinU1 * sinU2 + cosU1 * cosU2 * cosλ; //修正された距離の余弦を計算
+            σ = Math.atan2(sinσ, cosσ); //距離を計算
+            const sinα = cosU1 * cosU2 * sinλ / sinσ; //緯度の正弦を計算
+            cosSqα = 1 - sinα * sinα; //緯度の余弦の二乗を計算
+            cos2σM = cosσ - 2 * sinU1 * sinU2 / cosSqα; //中間計算
+            if (isNaN(cos2σM)) cos2σM = 0; // 赤道の場合
+            const C = f / 16 * cosSqα * (4 + f * (4 - 3 * cosSqα)); //中間計算
+            λP = λ; //前の方位角差の更新
+            λ = L + (1 - C) * f * sinα * (σ + C * sinσ * (cos2σM + C * cosσ * (-1 + 2 * cos2σM * cos2σM))); //新しい方位角差を計算
+        } while (Math.abs(λ - λP) > 1e-12 && --iterLimit > 0); //誤差が収束するか、反復上限を満たすまで反復
+
+        if (iterLimit === 0) return NaN; // 誤差が収束しなかった場合
+
+        /* 距離sを求めるための諸計算 */
+        const uSq = cosSqα * (a * a - b * b) / (b * b);
+        const A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
+        const B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
+        const Δσ = B * sinσ * (cos2σM + B / 4 * (cosσ * (-1 + 2 * cos2σM * cos2σM) - B / 6 * cos2σM * (-3 + 4 * sinσ * sinσ) * (-3 + 4 * cos2σM * cos2σM)));
+
+        const s = b * A * (σ - Δσ); //距離sを計算
+
+        return s;
+    }
+
+
 
     return (
         <>
-            <p>再生するページ</p>
+            <SimpleMap position={position} musics={musics} />
+            <Player />
         </>
     )
 }
---
+++ my-app/app/player/simpleMap.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+
+const defaultPosition = [34.81763, 135.36189]; // 
+
+function DynamicMarker({ position }) {
+  const map = useMap();
+
+  const [iconSize, setIconSize] = useState([25, 35]); // 初期アイコンサイズ
+
+  useEffect(() => {
+    const updateIconSize = () => {
+      const zoom = map.getZoom();
+      // ズームが10未満ならアイコンを非表示にする
+      if (zoom < 13) {
+        setIconSize([0, 0]);  // アイコンサイズをゼロにして消す
+      } else {
+        // ズームレベルに応じてアイコンサイズを変更
+        setIconSize([zoom * 1.5, zoom * 2]);  // ズームに基づいてアイコンサイズを調整
+      }
+    };
+
+
+    map.on('zoomend', updateIconSize);  // ズーム終了時にサイズを更新
+    updateIconSize();  // 初期化時にサイズを設定
+
+    return () => {
+      map.off('zoomend', updateIconSize);  // クリーンアップ
+    };
+  }, [map]);
+
+  // ミュージックアイコン設定
+  const musicIcon = L.icon({
+    iconUrl: 'https://img.icons8.com/?size=100&id=qJaxRD3kNRPO&format=png&color=000000',
+    iconSize,
+    iconAnchor: [12, 41],
+  });
+
+  return (
+    <Marker position={position} icon={musicIcon} minZoom={10} maxZoom={15}>
+      <Popup>曲名</Popup>
+    </Marker>
+  );
+}
+function RecenterMap({ position, isFollowing }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (position && isFollowing) {
+      map.setView(position, map.getZoom());
+    }
+  }, [position, isFollowing, map]);
+
+  return null;
+}
+
+export default function CurrentLocationMap({ position, musics }) {
+  const [isFollowing, setIsFollowing] = useState(true); // 追従モードの管理
+
+  // 現在地アイコンの設定
+  const myIcon = L.icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
+
+  if (!position) {
+    return <div>位置情報を取得中...</div>; // 位置情報が取得されるまでの表示
+  }
+
+  return (
+    <div style={{ position: "relative" }}>
+      <MapContainer center={position} zoom={13} style={{ height: 'calc(100vh - 128px)', width: '100%' }}>
+        <TileLayer url="https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png" />
+        <RecenterMap position={position} isFollowing={isFollowing} />
+        <Marker position={position} icon={myIcon}>
+          <Popup>現在地</Popup>
+        </Marker>
+
+        {/* DynamicMarker コンポーネントを使用 */}
+        {Array.isArray(musics) ? musics.map((music, i) => (
+          <DynamicMarker key={i} position={[music.Latitude, music.Longitude]} />
+        )) : null}
+
+      </MapContainer>
+
+      {/* 追従モードの切り替えボタン */}
+      <button
+        onClick={() => setIsFollowing(!isFollowing)}
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          right: "10px",
+          zIndex: 1000,
+          background: "white",
+          padding: "10px",
+          borderRadius: "5px",
+          cursor: "pointer"
+        }}
+      >
+        {isFollowing ? "追従ON" : "追従OFF"}
+      </button>
+    </div>
+
+  );
+}
